cf9558e63d536fe30ea61ec7ab852a48089fede9
Mathieu Derouet <22504262@me.uvsq.fr>
2026-02-26 12:06:01 +0100

    ajout fonction soustraction.

--- pycalc.py
+++ pycalc.py
@@ -4,12 +4,17 @@ op1 = float(sys.argv[1])
 op = sys.argv[2]
 op2 = float(sys.argv[3])
 
-def addition(x : int,y : int)->int:
+def addition(x : float,y : float)->float:
     a = x + y
     return a
 
 if op == "+":
     addition(op1,op2)
 
+def soustraction(x : float,y : float)->float:
+    s = x - y
+    return s
 
+if op == "-":
+    soustraction(op1,op2)
 
